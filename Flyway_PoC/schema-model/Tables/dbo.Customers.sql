74a8d7d54e5be08093e3347504ebcde21c84e2a7
markocoha <101259528+markocoha@users.noreply.github.com>
2022-07-29 15:46:52 +0200

    Twitter collumn

--- Flyway_PoC/schema-model/Tables/dbo.Customers.sql
+++ Flyway_PoC/schema-model/Tables/dbo.Customers.sql
@@ -3,6 +3,7 @@ CREATE TABLE [dbo].[Customers]
 [ID] [int] NOT NULL,
 [Name] [nvarchar] (50) NULL,
 [LName] [nvarchar] (50) NULL,
-[Address] [nvarchar] (500) NULL
+[Address] [nvarchar] (500) NULL,
+[Twitter] [nvarchar] (50) NULL
 )
 GO
