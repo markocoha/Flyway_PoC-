bb47c0d42f2b19cd38098e22ffc4395c202b05a5
markocoha <101259528+markocoha@users.noreply.github.com>
2022-07-29 14:34:39 +0200

    Expanded

--- Flyway_PoC/schema-model/Tables/dbo.TEsting_table.sql
+++ Flyway_PoC/schema-model/Tables/dbo.TEsting_table.sql
@@ -2,6 +2,7 @@ CREATE TABLE [dbo].[Testing_table]
 (
 [Testing] [nchar] (10) NULL,
 [Testing2] [int] NULL,
-[Testing3] [nvarchar] (50) NULL
+[Testing3] [nvarchar] (50) NULL,
+[Testing4] [nchar] (10) NULL
 )
 GO
